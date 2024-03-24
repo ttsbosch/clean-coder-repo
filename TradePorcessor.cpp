d5f4d5d523dc99c7b4892b691ae2476ce4591215
Venugopal K <venu.shastri@gmail.com>
2024-03-24 17:18:32 +0530

    Update TradePorcessor.cpp

--- TradePorcessor.cpp
+++ TradePorcessor.cpp
@@ -1,7 +1,7 @@
 #include "TradeProcessor.h"
-void TradeProcessor::ProcessTrades(std::istream& stream)
+void Processor::Process(std::istream& stream)
     {
-        
+
         std::vector<std::string> items;
         std::string item;
         while (std::getline(stream, item))
@@ -9,17 +9,17 @@ void TradeProcessor::ProcessTrades(std::istream& stream)
             items.push_back(item);
         }
 
-        std::vector<TradeRecord> records;
+        std::vector<TR> objects;
         int lineCount = 1;
         for (const auto& item : items)
         {
-            std::vector<std::string> fields ;
-          
+            std::vector<std::string> fields;
+
             std::string t;
             std::istringstream tStream(item);
             while (std::getline(tStream, t, ','))
             {
-            fields.push_back(t);
+                fields.push_back(t);
             }
             if (fields.size() != 3)
             {
@@ -31,41 +31,41 @@ void TradeProcessor::ProcessTrades(std::istream& stream)
                 std::cout << "WARN: Trade currencies on line " << lineCount << " malformed: '" << fields[0] << "'" << std::endl;
                 continue;
             }
-            int tradeAmount;
-            if (!TryParseInt(fields[1], tradeAmount))
+            int tam;
+            if (!intGetFromString(fields[1], tam))
             {
                 std::cout << "WARN: Trade amount on line " << lineCount << " not a valid integer: '" << fields[1] << "'" << std::endl;
             }
-            double tradePrice;
-            if (!TryParseDouble(fields[2], tradePrice))
+            double tp;
+            if (!toDouble(fields[2], tp))
             {
                 std::cout << "WARN: Trade price on line " << lineCount << " not a valid decimal: '" << fields[2] << "'" << std::endl;
             }
             std::string scc = fields[0].substr(0, 3);
             std::string dcc = fields[0].substr(3, 3);
             // calculate values
-            TradeRecord trade;
-            trade.SourceCurrency = scc;
-            trade.DestinationCurrency = dcc;
-            trade.Lots = tradeAmount / LotSize;
-            trade.Price = tradePrice;
-            records.push_back(trade);
+            TR  obj;
+            obj.SC = scc;
+            obj.DC = dcc;
+            obj.Lots = tam / LotSize;
+            obj.Price = tp;
+            objects.push_back(obj);
             lineCount++;
         }
 
-        std::ostringstream xmlStream;
-        xmlStream << "<TradeRecords>" << std::endl;
-        for (const auto& record : records) {
-        xmlStream << "\t<TradeRecord>" << std::endl;
-        xmlStream << "\t\t<SourceCurrency>" << record.SourceCurrency << "</SourceCurrency>" << std::endl;
-        xmlStream << "\t\t<DestinationCurrency>" << record.DestinationCurrency << "</DestinationCurrency>" << std::endl;
-        xmlStream << "\t\t<Lots>" << record.Lots << "</Lots>" << std::endl;
-        xmlStream << "\t\t<Price>" << record.Price << "</Price>" << std::endl;
-        xmlStream << "\t</TradeRecord>" << std::endl;
-       }
-        xmlStream << "</TradeRecords>";
-        std::string xmlData=xmlStream.str();
+        std::ostringstream xStream;
+        xStream << "<TradeRecords>" << std::endl;
+        for (const auto& record : objects) {
+            xStream << "\t<TradeRecord>" << std::endl;
+            xStream << "\t\t<SourceCurrency>" << record.SC << "</SourceCurrency>" << std::endl;
+            xStream << "\t\t<DestinationCurrency>" << record.DC << "</DestinationCurrency>" << std::endl;
+            xStream << "\t\t<Lots>" << record.Lots << "</Lots>" << std::endl;
+            xStream << "\t\t<Price>" << record.Price << "</Price>" << std::endl;
+            xStream << "\t</TradeRecord>" << std::endl;
+        }
+        xStream << "</TradeRecords>";
+        std::string xData = xStream.str();
         std::ofstream outFile("output.xml"); // Output XML file
-        outFile << xmlData;
-        std::cout << "INFO: " << records.size() << " trades processed" << std::endl;
+        outFile << xData;
+        std::cout << "INFO: " << objects.size() << " trades processed" << std::endl;
     }
